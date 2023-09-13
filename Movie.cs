979e4f8964a87b40ecc03a82160ae78b0d869fb7
42null <averyparanoiduser@gmail.com>
2023-09-13 12:52:16 -0500

    Most of header built up with basic centering (need to switch second part to string literals) and working title (year) seperation

--- Movie.cs
+++ Movie.cs
@@ -1,27 +1,48 @@
 public class Movie : IEquatable<Movie>
 {
-    // Can add set later if editing answer is 
-    public int id { get; }
+    //TODO: Make programatic in future versions
+    public const bool CONVERT_GREGORIAN_TO_HUMAN_ERA = false;
+    public const byte YEAR_SPACE_FOR_DIGIT_PLACES = CONVERT_GREGORIAN_TO_HUMAN_ERA? 5 : 4;
+    public const bool ALL_INPUT_YEARS_ARE_HUMAN_ERA = false;
+
+    public int id { get; }// Can add set later if editing answer is implemented
 
     public string title { get; }
+
+    public int year{ get; }//Allowed to be negative in case of using other year system other then gregorian
 
     public string[] genres{ get; }
 
     public Movie(int id, string title, /*params*/ string[] genres){//Cannot have "params" with overloaded method also uses strings there
         this.id = id;
-        this.title = title;
+        title = title.Trim();
+        //If title follows specifc format "______(####)", seperate out year
+        short lastOpeningParenthesieTitleIndex = (short)title.LastIndexOf("(");
+        short lastClosingParenthesieTitleIndex = (short)title.LastIndexOf(")");
+        // TODO: Decide to throw error if date is not included, it is not a field and decided as optional, not throwing/logging error for now
+        if(lastClosingParenthesieTitleIndex == title.Length-1 //Closing parenthesies must be at the end of string, also checks for Last is ")...(" instead of "(...)", determine not including a title
+           && lastClosingParenthesieTitleIndex - lastOpeningParenthesieTitleIndex-2 > 1 && lastClosingParenthesieTitleIndex - lastOpeningParenthesieTitleIndex - 2 <= YEAR_SPACE_FOR_DIGIT_PLACES //Number of charaters taking up must be at least 1, max YEAR_SPACE_FOR_DIGIT_PLACES, else determine does not include a title
+           && short.TryParse(title.Substring(lastOpeningParenthesieTitleIndex+1,lastClosingParenthesieTitleIndex-lastOpeningParenthesieTitleIndex-1), out short year)
+           ){
+            this.title = title.Substring(0,title.Length-(title.Length-lastOpeningParenthesieTitleIndex)).TrimEnd();//Beginning has already been trimmed, only need to check the end
+            if(CONVERT_GREGORIAN_TO_HUMAN_ERA && !ALL_INPUT_YEARS_ARE_HUMAN_ERA && year < 10_000){//Assumes that input is gregorian, unless year is at least 10,000
+                this.year = 10_000 + year;
+            }else{
+                this.year = year;
+            }
+        }else{//Particular title does contain year, skip removal from title
+            this.title = title;
+            // this.year = 0;
+        }
         this.genres = genres;
     }
-    // public Movie(int id, string title, string genreStringList, string genreStringDelimeter):this(
-    //     Movie(id, title, genreStringList.Split(genreStringDelimeter)))
-    // {}
     public Movie(int id, string title, string genreStringList, string genreStringDelimiter)
         : this(id, title, genreStringList.Split(genreStringDelimiter))
     {}
 
     public override string ToString()
     {
-        return "ID: " + id + "   Title: " + title;
+        return "Id: " + id + " Title: " + title + " Year: " + year;
     }
     public override bool Equals(object obj)
     {
--- Program.cs
+++ Program.cs
@@ -104,7 +104,11 @@ void displayMoviesFromFile(string dataPath)
         return;
     }
     List<Movie> movies = new List<Movie>();
-    int lineNumber = 1;
+    // Info
+    uint lineNumber = 1;//Should never be negative
+    // Metrics
+    ushort longestTitle = 0;
+
     while (!sr.EndOfStream)
     {
         bool recordIsBroken = true;
@@ -113,8 +117,8 @@ void displayMoviesFromFile(string dataPath)
         string[] movieParts = line.Split(DELIMETER_1);
         if (movieParts.Length > 3 && (line.Substring(line.IndexOf(DELIMETER_1)).Split(START_END_TITLE_WITH_DELIMETER1_INDICATOR).Length - 1 >= 2))
         {//Assume first that quotation marks are used to lower
-            int indexOfFirstDelimeter1 = line.IndexOf(DELIMETER_1) + 1;
-            int indexOfLastDelimeter1 = line.Substring(indexOfFirstDelimeter1).LastIndexOf(DELIMETER_1);
+            ushort indexOfFirstDelimeter1 = (ushort)(line.IndexOf(DELIMETER_1) + 1);//Can be ushort as line above makes sure cannot be -1
+            ushort indexOfLastDelimeter1 = (ushort)line.Substring(indexOfFirstDelimeter1).LastIndexOf(DELIMETER_1);//Can be ushort as line above makes sure cannot be -1
             movieParts[1] = line.Substring(indexOfFirstDelimeter1, indexOfLastDelimeter1).Replace(START_END_TITLE_WITH_DELIMETER1_INDICATOR, "");
             movieParts[2] = movieParts[movieParts.Length - 1];//Get last element that was split using delimeter #1
             movieParts = new string[] { movieParts[0], movieParts[1], movieParts[2] };
@@ -149,26 +153,59 @@ void displayMoviesFromFile(string dataPath)
         {
             Movie movie = new Movie(movieId, movieTitle, genres, DELIMETER_2);
             movies.Add(movie);
-            Console.WriteLine(movie);
+            // Console.WriteLine(movie);
+            // Update metrics
+            longestTitle = Math.Max(longestTitle, (ushort) movie.title.Length);
         }
 
-        // string[] secondary = line.Substring(line.IndexOf(DELIMETER_1) + 1).Split(DELIMETER_2);
+        // Update helpers
         lineNumber++;
+        if(lineNumber > 99){
+            break;
+        }
     }
     sr.Close();
+    // After list is created, display results to user.
+    string headerDividerLine     = "+";
+    string headerTitlesLine = "Movie Title";
 
-    //     // Caculate just once
-    //     string weekHeader = $" {DayOfWeek.Sunday.ToString().Substring(0,numPlacesInColumnAfterDividerDays),numPlacesInColumnAfterDividerDays} {DayOfWeek.Monday.ToString().Substring(0,numPlacesInColumnAfterDividerDays),numPlacesInColumnAfterDividerDays} {DayOfWeek.Tuesday.ToString().Substring(0,numPlacesInColumnAfterDividerDays),numPlacesInColumnAfterDividerDays} {DayOfWeek.Wednesday.ToString().Substring(0,numPlacesInColumnAfterDividerDays),numPlacesInColumnAfterDividerDays} {DayOfWeek.Thursday.ToString().Substring(0,numPlacesInColumnAfterDividerDays),numPlacesInColumnAfterDividerDays} {DayOfWeek.Friday.ToString().Substring(0,numPlacesInColumnAfterDividerDays),numPlacesInColumnAfterDividerDays} {DayOfWeek.Saturday.ToString().Substring(0,numPlacesInColumnAfterDividerDays),numPlacesInColumnAfterDividerDays}";
-    //     string calcHeader = $" {"Total".Substring(0,numPlacesInColumnAfterDividerCaculated),numPlacesInColumnAfterDividerCaculated} {"Avg".Substring(0,numPlacesInColumnAfterDividerCaculated),numPlacesInColumnAfterDividerCaculated}";
-    //     string underLineHeader = "";
-    //     for (int i = 0; i < 7; i++)
+    headerTitlesLine = string.Format($"{{0,{(longestTitle+headerTitlesLine.Length)/2}}}", headerTitlesLine);
+    headerTitlesLine = "|"+headerTitlesLine;
+    for(int i=headerTitlesLine.Length; i<=longestTitle; i++){ headerTitlesLine += " "; }
+    headerTitlesLine += "|";
+    headerTitlesLine += "Year";
+    for(int i=headerTitlesLine.Length; i<=Movie.YEAR_SPACE_FOR_DIGIT_PLACES; i++){ headerTitlesLine += " "; }
+    headerTitlesLine += "|";
+
+    for(int i=0; i<longestTitle; i++){ headerDividerLine += "-"; }
+    headerDividerLine += "+";
+    for(int i=0; i<Movie.YEAR_SPACE_FOR_DIGIT_PLACES; i++){ headerDividerLine += "-"; }
+    headerDividerLine += "+";
+
+    // Display header
+    Console.WriteLine(headerDividerLine);
+    Console.WriteLine(headerTitlesLine);
+    Console.WriteLine(headerDividerLine);
+
+
+    foreach (Movie movie in movies)
+    {
+        Console.WriteLine(string.Format($"{{0,-{longestTitle}}}", movie.title)+":"+movie.year);
+    }
+
+
+    // string weekHeader = $" {DayOfWeek.Sunday.ToString().Substring(0,numPlacesInColumnAfterDividerDays),numPlacesInColumnAfterDividerDays}";
+    // string calcHeader = $" {"Total".Substring(0,numPlacesInColumnAfterDividerCaculated),numPlacesInColumnAfterDividerCaculated} {"Avg".Substring(0,numPlacesInColumnAfterDividerCaculated),numPlacesInColumnAfterDividerCaculated}";
+    // string underLineHeader = "";
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     underLineHeader += " ";
+    //     for (int j = 0; j < numPlacesInColumnAfterDividerDays; j++)
     //     {
-    //         underLineHeader += " ";
-    //         for (int j = 0; j < numPlacesInColumnAfterDividerDays; j++)
-    //         {
-    //             underLineHeader += underLinePattern;
-    //         }
+    //         underLineHeader += underLinePattern;
     //     }
+    // }
+
     //     for (int i = 0; i < 2; i++)
     //     {
     //         underLineHeader += " ";
